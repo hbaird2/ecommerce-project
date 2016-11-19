48b7746acf061fe67137ad702f5eac9c0fa1a524
rachel baird <rachelbaird@Macintosh-4.local>
2016-11-19 13:22:16 -0700

    try get route

--- back-end/server.js
+++ back-end/server.js
@@ -9,7 +9,10 @@ var port = process.env.PORT || config.port;
 var massiveInstance = massive.connectSync({connectionString : connectionString})
 
 var app = module.exports = express();
-app.use(express.static('../front-end'));
+// app.use(express.static('../front-end'));
+app.get('/', function (req, res) {
+  res.sendFile('../front-end/index.html')
+})
 app.use(bodyParser.json());
 app.set('db', massiveInstance);
 var controller = require('./dataCtrl.js');
