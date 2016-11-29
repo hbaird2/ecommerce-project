58e81db5a8e282fc2970a06123252374be4c86f7
rachel baird <rachelbaird@Macintosh-4.local>
2016-11-28 20:37:49 -0700

    making changes for heroku postgres

--- back-end/server.js
+++ back-end/server.js
@@ -3,22 +3,41 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var massive = require('massive');
 // var config = require('../config.js')
-var connectionString = "postgres://rachelbaird@localhost/ecommerce";
+// uncomment to run local
+// var connectionString = "postgres://rachelbaird@localhost/ecommerce";
 var port = process.env.PORT || 8888;
+
 
 var massiveInstance = massive.connectSync({connectionString : connectionString})
 
 var app = module.exports = express();
-// app.use(express.static('../front-end'));
-app.get('/', function (req, res) {
-  res.sendFile('../front-end/index.html')
-})
+app.use(express.static('front-end'));
+// app.get('/', function (req, res) {
+//   res.sendFile('../front-end/index.html')
+// })
 app.use(bodyParser.json());
-app.set('db', massiveInstance);
+// uncomment to run local
+// app.set('db', massiveInstance);
 var controller = require('./dataCtrl.js');
 
+// uncomment to run local
+// app.get('/api/products', controller.getAll);
 
-app.get('/api/products', controller.getAll);
+// heroku postgres
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
 
 app.listen(port, function(){
   console.log("Successfully listening on : " + port)
--- front-end/js/dataService.js
+++ front-end/js/dataService.js
@@ -3,7 +3,8 @@ var ourService = angular.module('ecommerce').service('dataService', function($ht
   this.getProduct = function() {
     return $http({
       method: 'GET',
-      url: '/api/products'
+      // url: '/api/products'
+      url: '/db'
     }).then(function(response){
       return response.data;
     }, function(error){
