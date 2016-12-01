847124aa56d9698772ffba1374f64c4e87d60060
rachel baird <rachelbaird@Macintosh-4.local>
2016-11-30 20:57:19 -0700

    connect db

--- back-end/server.js
+++ back-end/server.js
@@ -5,7 +5,7 @@ var massive = require('massive');
 var config = require('../config.js')
 // uncomment to run local
 // var connectionString = "postgres://rachelbaird@localhost/ecommerce";
-var connectionString = "DATABASE_URL";
+var connectionString = "process.env.DATABASE_URL";
 var port = process.env.PORT || 8888;
 
 
