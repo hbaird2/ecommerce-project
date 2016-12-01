64d2130dcc1945482764002b77b95ccb18339573
rachel baird <rachelbaird@Macintosh-4.local>
2016-11-30 21:15:21 -0700

    connect

--- back-end/server.js
+++ back-end/server.js
@@ -22,22 +22,24 @@ app.set('db', massiveInstance);
 var controller = require('./dataCtrl.js');
 
 // uncomment to run local
-app.get('/api/products', controller.getAll);
+// app.get('/api/products', controller.getAll);
 
 // heroku postgres
-// var pg = require('pg');
+var pg = require('pg');
+pg.defaults.ssl = true;
 
-// app.get('/db', function (request, response) {
-//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//     client.query('SELECT * FROM test_table', function(err, result) {
-//       done();
-//       if (err)
-//        { console.error(err); response.send("Error " + err); }
-//       else
-//        { response.render('pages/db', {results: result.rows} ); }
-//     });
-//   });
-// });
+app.get('/api/products', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT * FROM product;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+});
 
 
 app.listen(port, function(){
