87f3ee173c027aadbe85f05424583f0f4a4ac07c
rachel baird <rachelbaird@Macintosh-4.local>
2016-12-06 19:30:37 -0700

    blah blah

--- back-end/server.js
+++ back-end/server.js
@@ -26,11 +26,7 @@ var controller = require('./dataCtrl.js');
 // app.get('/', function(request, response) {
 // response.send('Hello World!');
 // });
-app.get('/', function(req, res, next){
 
-    res.sendStatus(200);
-
-});
 
 app.get('/api/products', controller.getAll);
 
