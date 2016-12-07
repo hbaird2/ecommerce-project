c5762e1a473a8c40b8c06594b4ee91a032f27291
rachel baird <rachelbaird@Macintosh-4.local>
2016-12-06 20:07:14 -0700

    hi

--- server.js
+++ server.js
@@ -9,23 +9,22 @@ var port = process.env.PORT || 8888;
 var app = module.exports = express();
 var connectionString = process.env.DATABASE_URL;
 
+
 var massiveInstance = massive.connectSync({connectionString : connectionString})
 
-// app.use(express.static('/front-end'));
-// app.use(express.static('../front-end'));
+app.use(express.static('./front-end'));
 // app.get('/', function (req, res) {
-//   res.sendFile('/front-end/index.html')
+//   res.sendFile('../front-end/index.html')
 // })
 app.use(bodyParser.json());
 
 app.set('db', massiveInstance);
-var controller = require('back-end/dataCtrl.js');
+var controller = require('./back-end/dataCtrl.js');
 
 // uncomment to run local
 app.get('/', function(request, response) {
 response.send('Hello World!');
 });
-
 
 app.get('/api/products', controller.getAll);
 
